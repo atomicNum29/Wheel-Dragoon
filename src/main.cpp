4a31ade608cd001dc8d576a769b5a37a4ab911a8
Bak siu <atomicw63.546@gmail.com>
2026-01-17 16:01:46 +0900

    Refactor main.cpp to unify speed control logic and update pin assignments for PWM decoding

--- src/main.cpp
+++ src/main.cpp
@@ -2,45 +2,50 @@
 #include <SPI.h>
 #include "MCP41100.hpp"
 
-const int left_speed_controller_pin = 5;
-const int right_speed_controller_pin = 6;
+const int w_speed_controller_pin = 5;
+const int v_speed_controller_pin = 6;
 const int brake_controller_pin = 7;
 
 const int left_speed_control_pin = 9;
 const int right_speed_control_pin = 10;
 
-const int left_dir_control_pin = 11;
-const int right_dir_control_pin = 12;
-const int brake_control_pin = 13;
-const int stop_control_pin = 14;
+const int left_dir_control_pin = 12;
+const int left_brake_control_pin = 13;
+const int right_dir_control_pin = 14;
+const int right_brake_control_pin = 15;
+
+const double W = 0.42; // m
+const double R = 0.14; // m
 
 MCP41100 lefts(left_speed_control_pin);
 MCP41100 rights(right_speed_control_pin);
 
-volatile unsigned int left_pulseWidth = 0;
-volatile unsigned int right_pulseWidth = 0;
+volatile unsigned int v_pulseWidth = 0;
+volatile unsigned int w_pulseWidth = 0;
 volatile unsigned int isbraked = true;
 
-void left_decodePWM();
-void right_decodePWM();
+void v_decodePWM();
+void w_decodePWM();
 void brake_decodePWM();
 
 void setup()
 {
+    Serial.begin(115200);
+
     lefts.begin();
     rights.begin();
 
-    attachInterrupt(digitalPinToInterrupt(left_speed_controller_pin), left_decodePWM, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(right_speed_controller_pin), right_decodePWM, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(v_speed_controller_pin), v_decodePWM, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(w_speed_controller_pin), w_decodePWM, CHANGE);
     attachInterrupt(digitalPinToInterrupt(brake_controller_pin), brake_decodePWM, CHANGE);
 
     pinMode(left_dir_control_pin, OUTPUT);
+    pinMode(left_brake_control_pin, OUTPUT);
     pinMode(right_dir_control_pin, OUTPUT);
-    pinMode(brake_control_pin, OUTPUT);
-    pinMode(stop_control_pin, OUTPUT);
+    pinMode(right_brake_control_pin, OUTPUT);
 
-    digitalWrite(stop_control_pin, LOW);
-    digitalWrite(brake_control_pin, LOW);
+    digitalWrite(left_brake_control_pin, HIGH);
+    digitalWrite(right_brake_control_pin, HIGH);
     digitalWrite(left_dir_control_pin, LOW);
     digitalWrite(right_dir_control_pin, HIGH);
 }
@@ -49,50 +54,79 @@ void loop()
 {
     if (isbraked)
     {
-        digitalWrite(brake_control_pin, HIGH);
+        digitalWrite(left_brake_control_pin, HIGH);
+        digitalWrite(right_brake_control_pin, HIGH);
     }
     else
     {
-        digitalWrite(brake_control_pin, LOW);
+        digitalWrite(left_brake_control_pin, LOW);
+        digitalWrite(right_brake_control_pin, LOW);
     }
-    digitalWrite(stop_control_pin, HIGH);
 
-    unsigned int left_speed = left_pulseWidth;
-    unsigned int right_speed = right_pulseWidth;
-    if (left_speed < 1500)
+    double v_velocity = v_pulseWidth;
+    double w_velocity = w_pulseWidth;
+
+    v_velocity -= 1500; // 양수면 전진
+    w_velocity -= 1500; // 양수면 좌선회 (우측 바퀴가 +)
+
+    v_velocity /= 250; // -500~500 범위를 -2~2 범위로 줄임. (m/s)
+    w_velocity /= 100; // -500~500 범위를 -5~5 범위로 줄임. (rad/s)
+
+    double left_velocity = 0;
+    double right_velocity = 0;
+
+    left_velocity = v_velocity - w_velocity * W / 2;
+    right_velocity = v_velocity + w_velocity * W / 2;
+
+    int left_control_signal = left_velocity * 75;
+    int right_control_signal = right_velocity * 75;
+
+    // Serial.print(">v:");
+    // Serial.println(v_velocity);
+    // Serial.print(">w:");
+    // Serial.println(w_velocity);
+    // Serial.print(">left_velocity:");
+    // Serial.println(left_velocity);
+    // Serial.print(">right_velocity:");
+    // Serial.println(right_velocity);
+    // Serial.print(">left_control_signal:");
+    // Serial.println(left_control_signal);
+    // Serial.print(">right_control_signal:");
+    // Serial.println(right_control_signal);
+    // delay(10);
+
+    if (left_control_signal < 0)
     {
         digitalWrite(left_dir_control_pin, LOW);
-        left_speed = 1500 + 1500 - left_speed;
+        left_control_signal *= -1;
     }
     else
     {
         digitalWrite(left_dir_control_pin, HIGH);
     }
-    if (right_speed < 1500)
+
+    if (right_control_signal < 0)
     {
         digitalWrite(right_dir_control_pin, HIGH);
-        right_speed = 1500 + 1500 - right_speed;
+        right_control_signal *= -1;
     }
     else
     {
         digitalWrite(right_dir_control_pin, LOW);
     }
 
-    left_speed = map(left_speed, 1500, 2000, 0, 100);
-    right_speed = map(right_speed, 1500, 2000, 0, 100);
-
-    lefts.setWiper(left_speed);
-    rights.setWiper(right_speed);
+    lefts.setWiper(left_control_signal);
+    rights.setWiper(right_control_signal);
 }
 
-void left_decodePWM()
+void v_decodePWM()
 {
     static unsigned long prevTime = 0;
     static unsigned long dataA[10] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
     static unsigned int idx = 0;
     static unsigned long sum = 15000;
 
-    if ((prevTime != 0) && !digitalRead(left_speed_controller_pin))
+    if ((prevTime != 0) && !digitalRead(v_speed_controller_pin))
     {
         unsigned long tmp = micros() - prevTime;
         if (tmp > 2000)
@@ -100,7 +134,7 @@ void left_decodePWM()
         sum += tmp - dataA[idx];
         dataA[idx] = tmp;
         idx = (idx + 1) % 10;
-        left_pulseWidth = sum / 10;
+        v_pulseWidth = sum / 10;
         prevTime = 0;
     }
     else
@@ -109,14 +143,14 @@ void left_decodePWM()
     }
 }
 
-void right_decodePWM()
+void w_decodePWM()
 {
     static unsigned long prevTime = 0;
     static unsigned long dataA[10] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
     static unsigned int idx = 0;
     static unsigned long sum = 15000;
 
-    if ((prevTime != 0) && !digitalRead(right_speed_controller_pin))
+    if ((prevTime != 0) && !digitalRead(w_speed_controller_pin))
     {
         unsigned long tmp = micros() - prevTime;
         if (tmp > 2000)
@@ -124,7 +158,7 @@ void right_decodePWM()
         sum += tmp - dataA[idx];
         dataA[idx] = tmp;
         idx = (idx + 1) % 10;
-        right_pulseWidth = sum / 10;
+        w_pulseWidth = sum / 10;
         prevTime = 0;
     }
     else
