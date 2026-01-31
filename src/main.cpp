6c906a86f0948830159175092295bed05b5323a0
Bak siu <atomicw63.546@gmail.com>
2026-01-31 22:42:52 +0900

    Refactor main.cpp to update motor control feedback pin assignments, add wheel pulse ISR functions, and implement PWM frequency and resolution settings

--- src/main.cpp
+++ src/main.cpp
@@ -18,15 +18,22 @@ const int lr_speed_control_pin = 4;
 const int rr_speed_control_pin = 5;
 const int rf_speed_control_pin = 6;
 
+volatile unsigned int led_state = 0;
+
 // motor control feedback pins, 60 PPR encoders
-const int lf_wheel_pulse_pin = 7;
-const int lr_wheel_pulse_pin = 8;
-const int rr_wheel_pulse_pin = 9;
-const int rf_wheel_pulse_pin = 10;
-// volatile unsigned int lf_wheel_pulse_count = 0;
-// volatile unsigned int lr_wheel_pulse_count = 0;
-// volatile unsigned int rr_wheel_pulse_count = 0;
-// volatile unsigned int rf_wheel_pulse_count = 0;
+const int lf_wheel_pulse_pin = 9;
+const int lr_wheel_pulse_pin = 10;
+const int rr_wheel_pulse_pin = 11;
+const int rf_wheel_pulse_pin = 12;
+volatile unsigned int lf_wheel_pulse_count = 0;
+volatile unsigned int lr_wheel_pulse_count = 0;
+volatile unsigned int rr_wheel_pulse_count = 0;
+volatile unsigned int rf_wheel_pulse_count = 0;
+void lf_wheel_pulse_ISR();
+void lr_wheel_pulse_ISR();
+void rr_wheel_pulse_ISR();
+void rf_wheel_pulse_ISR();
+void clear_wheel_pulse_counts();
 
 // motor direction control pins
 const int left_dir_control_pin = 14;
@@ -36,22 +43,33 @@ const int right_dir_control_pin = 15;
 const double W = 0.42; // m
 const double R = 0.14; // m
 
-MCP41100 lf_motor(lf_speed_control_pin);
-MCP41100 lr_motor(lr_speed_control_pin);
-MCP41100 rf_motor(rf_speed_control_pin);
-MCP41100 rr_motor(rr_speed_control_pin);
-
 void setup()
 {
     Serial.begin(115200);
 
-    lf_motor.begin();
-    lr_motor.begin();
-    rf_motor.begin();
-    rr_motor.begin();
-
+    pinMode(v_speed_controller_pin, INPUT);
+    pinMode(w_speed_controller_pin, INPUT);
     attachInterrupt(digitalPinToInterrupt(v_speed_controller_pin), v_decodePWM, CHANGE);
     attachInterrupt(digitalPinToInterrupt(w_speed_controller_pin), w_decodePWM, CHANGE);
+
+    pinMode(lf_speed_control_pin, OUTPUT);
+    pinMode(lr_speed_control_pin, OUTPUT);
+    pinMode(rr_speed_control_pin, OUTPUT);
+    pinMode(rf_speed_control_pin, OUTPUT);
+
+    analogWriteFrequency(lf_speed_control_pin, 10000); // 10 kHz PWM
+    analogWriteFrequency(lr_speed_control_pin, 10000);
+    analogWriteFrequency(rr_speed_control_pin, 10000);
+    analogWriteFrequency(rf_speed_control_pin, 10000);
+
+    analogWriteResolution(10); // 10 bit resolution (0-1023)
+
+    attachInterrupt(digitalPinToInterrupt(lf_wheel_pulse_pin), lf_wheel_pulse_ISR, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(lr_wheel_pulse_pin), lr_wheel_pulse_ISR, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(rr_wheel_pulse_pin), rr_wheel_pulse_ISR, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(rf_wheel_pulse_pin), rf_wheel_pulse_ISR, CHANGE);
+
+    pinMode(LED_BUILTIN, OUTPUT);
 
     pinMode(left_dir_control_pin, OUTPUT);
     pinMode(right_dir_control_pin, OUTPUT);
@@ -75,8 +93,8 @@ void loop()
     left_velocity = v_velocity - w_velocity * W / 2;
     right_velocity = v_velocity + w_velocity * W / 2;
 
-    int left_control_signal = left_velocity * 75;
-    int right_control_signal = right_velocity * 75;
+    int left_control_signal = left_velocity * 300;
+    int right_control_signal = right_velocity * 300;
 
     // Serial.print(">v:");
     // Serial.println(v_velocity);
@@ -90,7 +108,15 @@ void loop()
     // Serial.println(left_control_signal);
     // Serial.print(">right_control_signal:");
     // Serial.println(right_control_signal);
-    // delay(10);
+    Serial.print(">lf_pulse:");
+    Serial.println(lf_wheel_pulse_count);
+    Serial.print(">lr_pulse:");
+    Serial.println(lr_wheel_pulse_count);
+    Serial.print(">rf_pulse:");
+    Serial.println(rf_wheel_pulse_count);
+    Serial.print(">rr_pulse:");
+    Serial.println(rr_wheel_pulse_count);
+    delay(10);
 
     if (left_control_signal < 0)
     {
@@ -112,10 +138,17 @@ void loop()
         digitalWrite(right_dir_control_pin, LOW);
     }
 
-    lf_motor.setWiper(left_control_signal);
-    lr_motor.setWiper(left_control_signal);
-    rf_motor.setWiper(right_control_signal);
-    rr_motor.setWiper(right_control_signal);
+    // Set control signals to motors
+    analogWrite(lf_speed_control_pin, constrain(left_control_signal, 0, 1023));
+    analogWrite(lr_speed_control_pin, constrain(left_control_signal, 0, 1023));
+    analogWrite(rf_speed_control_pin, constrain(right_control_signal, 0, 1023));
+    analogWrite(rr_speed_control_pin, constrain(right_control_signal, 0, 1023));
+    // lf_motor.setWiper(left_control_signal);
+    // lr_motor.setWiper(left_control_signal);
+    // rf_motor.setWiper(right_control_signal);
+    // rr_motor.setWiper(right_control_signal);
+
+    clear_wheel_pulse_counts();
 }
 
 void v_decodePWM()
@@ -164,4 +197,41 @@ void w_decodePWM()
     {
         prevTime = micros();
     }
+}
+
+void lf_wheel_pulse_ISR()
+{
+    lf_wheel_pulse_count++;
+    digitalWrite(LED_BUILTIN, led_state ^= 1);
+}
+
+void lr_wheel_pulse_ISR()
+{
+    lr_wheel_pulse_count++;
+    digitalWrite(LED_BUILTIN, led_state ^= 1);
+}
+
+void rr_wheel_pulse_ISR()
+{
+    rr_wheel_pulse_count++;
+    digitalWrite(LED_BUILTIN, led_state ^= 1);
+}
+
+void rf_wheel_pulse_ISR()
+{
+    rf_wheel_pulse_count++;
+    digitalWrite(LED_BUILTIN, led_state ^= 1);
+}
+
+void clear_wheel_pulse_counts()
+{
+    static unsigned int prev_t = 0;
+    unsigned int curr_t = millis();
+    if (curr_t - prev_t < 100)
+        return;
+    prev_t = curr_t;
+    lf_wheel_pulse_count = 0;
+    lr_wheel_pulse_count = 0;
+    rr_wheel_pulse_count = 0;
+    rf_wheel_pulse_count = 0;
 }
